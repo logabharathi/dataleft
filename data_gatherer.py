0d8f42d5dce65c51756c095b1ed1d1431dc3dd7d
Logabharathi Aruchamy <logabharathi@gmail.com>
2020-02-10 14:32:10 +0100

    Update the correct data gatherer for first news source

--- data_gatherer.py
+++ data_gatherer.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python3
+
 import requests
 from bs4 import BeautifulSoup
+from googletrans import Translator
 
 # Collect data from top news websites
 page = requests.get('https://ns7.tv/ta')
 
 soup = BeautifulSoup(page.text, 'html.parser')
+translator = Translator()
 
-list_text = soup.find(class_='head_news todaynews')
+list_text = soup.find_all(class_='headlinecard')
 
-print(list_text)
+for s in list_text:
+    time_of_news = s.find(class_='clockico')
+    if not("day" or "days" or "week") in time_of_news.text.strip():
+        news = s.find('p')
+        news_translated = translator.translate(news.text.encode("utf-8"))
+        print ("{} published {}".format(news_translated.text.strip(), time_of_news.text.strip()))
